d561a891572729845a12fee1bb5f5e41cd272de1
Brashkie <142468732+Brashkie@users.noreply.github.com>
2025-07-25 18:37:36 -0500

    HepeinBot-PRO

--- plugins/_config-features.js
+++ plugins/_config-features.js
@@ -1,17 +1,1 @@
-// config-features.js
-
-if (!global.db) global.db = {};
-if (!global.db.data) global.db.data = {};
-
-// Función para asegurar config por grupo
-export function getSettings(groupId) {
-    if (!global.db.data.settings[groupId]) {
-        global.db.data.settings[groupId] = {
-            antilink: false,
-            antiviewonce: false,
-            antispam: false,
-            // puedes agregar más
-        };
-    }
-    return global.db.data.settings[groupId];
-}
+/**/
