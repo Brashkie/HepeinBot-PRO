913d3bde33e30e5122c41e0abbc1735317113546
Brashkie <142468732+Brashkie@users.noreply.github.com>
2025-07-25 18:41:10 -0500

    HepeinBot-PRO

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -1,18 +1,1 @@
-const handler = async (conn, m, { participants }) => {
-    const nombres = await Promise.all(participants.map(async (p) => {
-        const nombre = await conn.getName(p.id);
-        return `⊹ @${nombre}`;
-    }));
-
-    await conn.sendMessage(m.chat, {
-        text: `╭━〔 👥 *Mencionando a todos* 〕━⬣\n\n${nombres.join('\n')}\n╰━━━━⬣`,
-        mentions: participants.map(p => p.id)
-    }, { quoted: m });
-};
-
-handler.command = /^tagall$/i;
-handler.group = true;
-handler.admin = true;
-handler.botAdmin = true;
-
-export default handler;
+/**/
