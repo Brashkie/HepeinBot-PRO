eb960ae59cdbdf31fb4988d3b5cc0e8ddf6c65b4
Brashkie <142468732+Brashkie@users.noreply.github.com>
2025-08-03 20:26:26 -0500

    HepeinBot-PRO

---
+++ plugins/_MID-GB.js
@@ -0,0 +1,15 @@
+import { es } from '../lib/multi-language/_default.js' 
+
+let handler = m => m
+handler.before = async function (m, { conn }) {
+let idioma  = global.db.data.users[m.sender].midLanguage
+let MID_GB
+
+if (idioma == "es") {
+MID_GB = es
+} else {
+MID_GB = mid || es
+}
+global.mid = MID_GB	
+}
+export default handler
