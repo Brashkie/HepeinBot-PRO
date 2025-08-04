ff843d720985f4f6077aae76b23c90210cebb23a
Brashkie <142468732+Brashkie@users.noreply.github.com>
2025-08-03 19:59:54 -0500

    HepeinBot-PRO

---
+++ plugins/propietario(a)-banchat.js
@@ -0,0 +1,12 @@
+//PARA GRUPOS
+
+let handler = async (m) => {
+global.db.data.chats[m.chat].isBanned = true
+m.reply(`${eg}𝙀𝙎𝙏𝙀 𝘾𝙃𝘼𝙏 𝙁𝙐𝙀 𝘽𝘼𝙉𝙀𝘼𝘿𝙊 🤑\n𝙉𝙊 𝙀𝙎𝙏𝘼𝙍𝙀 𝘿𝙄𝙎𝙋𝙊𝙉𝙄𝘽𝙇𝙀 𝙃𝘼𝙎𝙏𝘼 𝙌𝙐𝙀 𝙎𝙀𝘼 𝘿𝙀𝙎𝘽𝘼𝙉𝙀𝘼𝘿𝘼.`)
+}
+handler.help = ['banchat']
+handler.tags = ['owner']
+handler.command = /^banchat|bangata|banchat2$/i
+//handler.botAdmin = true
+handler.owner = true
+export default handler
